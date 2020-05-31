387b5189f9b65e4ad753b74e6688b8a41d0f7969
ppalone <pranjalalone@gmail.com>
2020-05-31 16:14:53 +0530

    Setup React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import Developers from './components/Developers';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Developers from './components/Developers/Developers';
 
 function App() {
   return (
     <div className="App">
-      <h1>DEVELOPERS</h1>
-      <Developers />
+      <Router>
+        <div>
+          <h2>Welcome to Gcoea Developers Website</h2>
+          <nav>
+            <ul>
+              <li><Link to={'/'}>Home</Link></li>
+              <li><Link to={'/about'}>About</Link></li>
+              <li><Link to={'/developers'}>Developers</Link></li>
+            </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/developers' component={Developers} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
