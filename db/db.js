0b8882e08e9fbcca5dd7be825d6ed1f730f19920
ppalone <pranjalalone@gmail.com>
2020-07-09 19:25:15 +0530

    Added separate file to store developers

---
+++ db/db.js
@@ -0,0 +1,18 @@
+module.exports = {
+    developers: [
+        {
+            "name": "Pranjal Alone",
+            "img": "https://avatars0.githubusercontent.com/u/52038311?s=400&v=4",
+            "github": "https://github.com/ppalone",
+            "linkedin": "https://in.linkedin.com/in/pranjal-alone",
+            "skills": ["javascript", "nodejs", "python", "react"]
+        },
+        {
+            "name": "Vaibhav Butale",
+            "img": "https://avatars0.githubusercontent.com/u/52038311?s=400&v=4",
+            "github": "https://github.com/ppalone",
+            "linkedin": "https://in.linkedin.com/in/pranjal-alone",
+            "skills": ["java", "c++", "c", "spring"]
+        }
+    ]
+}
--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 const express = require('express');
 const cors = require('cors'); 
 const app = express();
+const DB = require('./db/db');
 const PORT = process.env.PORT || 8000
 
 // Use cors
 app.use(cors());
 
 app.get('/api/developers', (req, res) => {
-    const developers = [
-        {
-            id: 1,
-            name: 'Pranjal Alone'
-        },
-        {
-            id: 2,
-            name: 'Vaibhav Butale'
-        },
-        {
-            id: 3,
-            name: 'Nilay Ingle'
-        },
-        {
-            id: 4,
-            name: 'Pruthviraj Gite'
-        }
-    ]
-    res.json(developers);
+    res.status(200).json(DB.developers);
 });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
