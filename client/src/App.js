f1349a9d332b773a361008b121d29360d69ffccb
Jabo <joelvincent.work@gmail.com>
2020-10-02 19:15:58 +0800

    ➕ Added projects page with github api integration

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Backdrop from './components/Backdrop';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Developers from './components/Developers/Developers';
+import Projects from './components/Projects/Projects';
 import Join from './components/Join/Join';
 import './App.css';
 
@@ -39,6 +40,7 @@ class App extends Component {
             <Route path="/" component={Home} exact />
             <Route path="/about" component={About} />
             <Route path="/developers" component={Developers} />
+            <Route path="/projects" component={Projects} />
             <Route path="/join" component={Join} />
           </Switch>
         </div>
--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
+
+    state = {
+        projects: null,
+        loaderMessage: "Loading..."
+    }
+
+    componentDidMount = async () => {
+        /**
+         * Retrieve the projects from Github
+         */
+        const response = await fetch('https://api.github.com/users/GCOEA-DEV/repos');
+        const projects = await response.json();
+        if(response.status === 200) {
+            this.setState({ projects });
+        } else {
+            this.setState({ loaderMessage: "Unable to retrive the projects. Try again" });
+        }
+
+    }
+
     render() {
+
+        const { projects, loaderMessage } = this.state;
+
         return (
-            <h1>All Projects go here 🚀</h1>
+            <>
+                <h1>Projects</h1>
+                <div className="developers">
+                    <div className="developers-header">
+                    <h2>PROJECTS 📦</h2>
+                    </div>
+                    <div className="developers-table">
+                        { projects === null ? <p>{ loaderMessage }</p> :
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Project Name</th>
+                                <th>Description</th>
+                                <th>Language</th>
+                            </tr>
+                            </thead>
+                        <tbody>
+                            {projects.map(project => (
+                                <tr>
+                                    <td><a target='_blank' href={project.html_url}>{project.name}</a></td>
+                                    <td>{project.description}</td>
+                                    <td>{project.language}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                        </table>   
+                    }
+                    </div>
+                </div>                
+            </>
         );
     }
 }
