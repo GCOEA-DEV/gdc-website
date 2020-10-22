e0b238e54a436c2d62e24e8c7df069cfe93df0df
ppalone <pranjalalone@gmail.com>
2020-10-22 19:17:28 +0530

    Fix: Add path module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 // Routes
