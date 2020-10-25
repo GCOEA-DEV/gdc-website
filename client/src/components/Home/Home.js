e12bac614b09250864d6041f481562fc09097162
ppalone <pranjalalone@gmail.com>
2020-10-25 19:41:29 +0530

    Added sidelinks, modified footer

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Rocket from './Rocket';
 import Footer from './Footer';
+import Links from './Links';
 
 import './Home.css';
 
@@ -9,6 +10,7 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
+        <Links />
         <div className="main">
           <Rocket />
         </div>
