f2f8694b129ed1d8cea77525b2681227162b1621
ppalone <pranjalalone@gmail.com>
2020-11-01 20:54:11 +0530

    Form changes and UI changes

--- client/src/components/Join/Form.js
+++ client/src/components/Join/Form.js
@@ -1,38 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Form.css';
 
-function Form() {
-  return (
-    <div className="form-div">
-      <form action="/api/sendlink" method="POST">
-        <div className="input-div">
-          <label>Name</label>
-        </div>
-        <div className="input-div">
-          <input
-            type="text"
-            name="name"
-            autoComplete="off"
-            placeholder="Name"
-          />
-        </div>
-        <div className="input-div">
-          <label>Email</label>
-        </div>
-        <div className="input-div">
-          <input
-            type="email"
-            name="email"
-            autoComplete="off"
-            placeholder="Email"
-          />
-        </div>
-        <div className="input-div">
-          <input type="submit" value="Send Invite Link" />
-        </div>
-      </form>
-    </div>
-  );
+class Form extends Component {
+
+  state = {
+    name: 'Pranjal',
+    email: 'pranjalalone@gmail.com',
+    loading: false,
+    message: ''
+  }
+
+  handleChange = (e) => {
+    // console.log(e.target.name);
+    if (e.target.name === 'name') {
+      this.setState({
+        name: e.target.value
+      })
+    }
+    if (e.target.name === 'email') {
+      this.setState({
+        email: e.target.value
+      })
+    }
+  }
+
+  handleFormSubmit = (e) => {
+    // alert('Form submitted');
+    // Prevent Refreshing of page
+    e.preventDefault();
+
+    this.setState({
+      loading: true
+    });
+
+    setTimeout(() => {
+      this.setState({
+        name: '',
+        email: '',
+        loading: false,
+        message: 'Your Response has been recorded!'
+      });
+    }, 3000);
+  }
+
+  disappear = () => {
+    this.setState({ message: '' });
+  }
+
+  render() {
+    return (
+      <div className="form-div">
+        {this.state.message.trim() && <div className='message' onAnimationEnd={this.disappear}>{this.state.message}</div>}
+        <form onSubmit={this.handleFormSubmit}>
+          <div className="input-div">
+            <label>Name</label>
+          </div>
+          <div className="input-div">
+            <input
+              type="text"
+              name="name"
+              autoComplete="off"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-div">
+            <label>Email</label>
+          </div>
+          <div className="input-div">
+            <input
+              type="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-div">
+            <input type="submit" value={this.state.loading ? 'Submitting . . .' : 'Send Invite Link'} disabled={!this.state.name.trim() || !this.state.email.trim() || this.state.loading} />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
