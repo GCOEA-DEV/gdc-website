c318ee29b1f16d6b36bffa4873d87b87f60dedb9
ppalone <pranjalalone@gmail.com>
2020-11-03 22:39:17 +0530

    Form submit

--- client/src/components/Join/Form.js
+++ client/src/components/Join/Form.js
@@ -4,59 +4,70 @@ import './Form.css';
 import CancelIcon from '@material-ui/icons/Cancel';
 
 class Form extends Component {
-
   state = {
     name: '',
     email: '',
     loading: false,
-    message: ''
-  }
+    message: '',
+  };
 
   handleChange = (e) => {
-    if (e.target.name === 'name') {
-      this.setState({
-        name: e.target.value
-      })
-    }
-    if (e.target.name === 'email') {
-      this.setState({
-        email: e.target.value
-      })
-    }
-  }
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  handleFormSubmit = (e) => {
+  handleFormSubmit = async (e) => {
     // alert('Form submitted');
     // Prevent Refreshing of page
     e.preventDefault();
 
-    this.setState({
-      loading: true
-    });
-
-    setTimeout(() => {
-      this.setState({
-        name: '',
-        email: '',
-        loading: false,
-        message: 'Your Response has been recorded!'
+    // Submit the data
+    try {
+      this.setState({ loading: true });
+      const res = await fetch('/api/mail', {
+        method: 'POST',
+        mode: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+        }),
       });
-    }, 3000);
-  }
+
+      if (res.status === 200) {
+        let data = await res.json();
+        this.setState({
+          loading: false,
+          message: data.message,
+          name: '',
+          email: '',
+        });
+      } else {
+        this.setState({ loading: false, message: 'Something went wrong :(' });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   removeMessage = () => {
-    this.setState({ message: '' })
-  }
+    this.setState({ message: '' });
+  };
 
   render() {
     return (
       <div className="form-div">
-        {this.state.message.trim() && <div className='message'>
-          <div>{this.state.message}</div>
-          <div>
-            <CancelIcon onClick={this.removeMessage} />
+        {this.state.message.trim() && (
+          <div className="message">
+            <div className="message-res">{this.state.message}</div>
+            <div>
+              <CancelIcon onClick={this.removeMessage} />
+            </div>
           </div>
-        </div>}
+        )}
         <form onSubmit={this.handleFormSubmit}>
           <div className="input-div">
             <label>Name</label>
@@ -85,7 +96,17 @@ class Form extends Component {
             />
           </div>
           <div className="input-div">
-            <input type="submit" value={this.state.loading ? 'Submitting . . .' : 'Send Invite Link'} disabled={!this.state.name.trim() || !this.state.email.trim() || this.state.loading} />
+            <input
+              type="submit"
+              value={
+                this.state.loading ? 'Submitting . . .' : 'Send Invite Link'
+              }
+              disabled={
+                !this.state.name.trim() ||
+                !this.state.email.trim() ||
+                this.state.loading
+              }
+            />
           </div>
         </form>
       </div>
